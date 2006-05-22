389e21aea3e2f4fdb20d95c2d7bf56189bebfd2d
rpeng <rpeng>
2006-05-22 18:45:31 +0000

    add registration of database format

---
+++ R/zzz.R
@@ -0,0 +1,19 @@
+.onLoad <- function(lib, pkg) {
+    pkgList <- c("methods", "filehash", "DBI", "RSQLite")
+
+    for(package in pkgList) {
+        if(!require(package, quietly = TRUE, character.only = TRUE))
+            stop(sQuote(package), " package required")
+    }
+    ## Regsiter 'filehash' database format
+    r <- list(create = createSQLite, initialize = initializeSQLite)
+    registerFormatDB("SQLite", r)
+}
+
+.onAttach <- function(lib, pkg) {
+    dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
+    msg <- paste(dcf[, "Title"], " (version ",
+                 as.character(dcf[, "Version"]), ")", sep = "")
+    writeLines(strwrap(msg))
+}
+
