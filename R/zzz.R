72f65e9b4b40099600a32cdf62f328c9b053110c
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-04-10 13:09:09 -0400

    Tweak startup message

--- R/zzz.R
+++ R/zzz.R
@@ -12,8 +12,8 @@
 
 .onAttach <- function(lib, pkg) {
     dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
-    msg <- gettextf("%s (version %s %s)", dcf[, "Title"],
+    msg <- gettextf("%s (%s %s)", dcf[, "Title"],
                     as.character(dcf[, "Version"]), dcf[, "Date"])
-    writeLines(strwrap(msg))
+    message(paste(strwrap(msg), collapse = "\n"))
 }
 
