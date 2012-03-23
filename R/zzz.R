0a42b5c08e352925f742970ad7c8f5d32232a4b2
Roger D. Peng [amelia] <rdpeng@gmail.com>
2012-03-23 17:32:30 -0400

    Don't load packages in startup code

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,4 @@
 .onLoad <- function(lib, pkg) {
-    pkgList <- c("methods", "filehash", "DBI", "RSQLite")
-
-    for(package in pkgList) {
-        if(!require(package, quietly = TRUE, character.only = TRUE))
-            stop(gettextf("'%s' package required", package))
-    }
     ## Register 'filehash' database format
     r <- list(create = createSQLite, initialize = initializeSQLite)
     registerFormatDB("SQLite", r)
@@ -14,6 +8,6 @@
     dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
     msg <- gettextf("%s (%s %s)", dcf[, "Title"],
                     as.character(dcf[, "Version"]), dcf[, "Date"])
-    message(paste(strwrap(msg), collapse = "\n"))
+    packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
 }
 
