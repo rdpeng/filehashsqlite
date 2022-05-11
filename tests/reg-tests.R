ef551817dfda93cf9746de606c809c79584d3b89
Roger D. Peng <rdpeng@jhu.edu>
2022-05-11 10:49:27 -0400

    Add more tests

--- tests/reg-tests.R
+++ tests/reg-tests.R
@@ -2,6 +2,7 @@ suppressPackageStartupMessages(library(filehashSQLite))
 
 dbCreate("test1", "SQLite")
 db <- dbInit("test1", "SQLite")
+db
 
 set.seed(234)
 val <- rnorm(1000)
@@ -12,5 +13,16 @@ x <- dbFetch(db, "a")
 str(x)
 
 stopifnot(identical(x, val))
+
+val2 <- runif(10)
+dbInsert(db, "b", val2)
+
+m <- dbMultiFetch(db, c("a", "b"))
+str(m)
+
+dbDelete(db, "a")
+dbList(db)
+dbExists(db, "a")
+dbExists(db, "b")
 
 dbDisconnect(db)
